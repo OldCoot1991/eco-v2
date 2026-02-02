1dd114b331ed99909357239824419d24c12edd3a
Ahmed <kochesokov1503@gmail.com>
2026-02-03 01:28:24 +0300

    удалил с главной страницы не нужные компоненты

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import { Hero } from "@/components/home/Hero/Hero";
-import { Services } from "@/components/home/Services/Services";
-import { News } from "@/components/home/News/News";
-import { InfoSection } from "@/components/home/InfoSection/InfoSection";
-import { GosuslugiSection } from "@/components/home/GosuslugiSection/GosuslugiSection";
 import { MobileApp } from "@/components/home/MobileApp/MobileApp";
 import { PaymentPartners } from "@/components/home/PaymentPartners/PaymentPartners";
 import { TrustedBy } from "@/components/home/TrustedBy/TrustedBy";
@@ -17,13 +12,8 @@ export const metadata = {
 export default function Home() {
   return (
     <main className={styles.mainLayout}>
-      <Hero />
-      <Services />
-
       <section className={styles.promoSection}>
         <div className={styles.container}>
-          <News />
-          <InfoSection />
           <MobileApp />
           <PaymentPartners />
         </div>
